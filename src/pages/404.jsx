ee8e61d47a250cecf0326a5cc9eb3d61005cae22
Javier Ignacio <javier.quinteros@inacapmail.cl>
2018-02-01 19:17:34 -0300

    Color Palette Changes

--- src/pages/404.jsx
+++ src/pages/404.jsx
@@ -2,10 +2,16 @@ import React from "react";
 import HeadlineWithFocus from '../components/HeadlineWithFocus';
 
 export default () => (
-  <div className="c-content-box">
+  <div className="c-content-box-centered">
     <HeadlineWithFocus>
-      404
+      You have found The 404 End
     </HeadlineWithFocus>
-    <p>There is no page you are looking for.</p>
+    <p>This is the end, beautiful friend</p>
+    <p>This is the end, my only friend,</p>
+    <p>the end.</p>
+    <p>Of our elaborate plans, the end.</p>   
+    <p>Of everything that stands, the end.</p>
+    <p>No safety or surprise, the end.</p>
+    <p>I'll never look into your eyes, again.</p>  
   </div>
 );
