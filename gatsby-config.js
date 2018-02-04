e7ea3f49b5c7cccee61193dc765c88a378726a12
Javier Ignacio <javier.quinteros@inacapmail.cl>
2018-02-03 22:08:32 -0300

    Manifest JSON Plugin

--- gatsby-config.js
+++ gatsby-config.js
@@ -22,6 +22,32 @@ module.exports = {
             options: {
                 trackingId: "UA-110088221-1"
             }
+        },
+        {
+            resolve: `gatsby-plugin-manifest`,
+            options: {
+                name: "Javier Ignacio as a Service",
+                short_name: "JiaaS",
+                start_url: "/",
+                background_color: "#f7f0eb",
+                theme_color: "#3998cf",
+                display: "minimal-ui",
+                icons: [{
+                        // Everything in /static will be copied to an equivalent
+                        // directory in /public during development and build, so
+                        // assuming your favicons are in /static/favicons,
+                        // you can reference them here
+                        src: `/favicon.ico`,
+                        sizes: `192x192`,
+                        type: `image/png`,
+                    },
+                    {
+                        src: `/favicon.ico`,
+                        sizes: `512x512`,
+                        type: `image/png`,
+                    },
+                ],
+            },
         }
     ]
 }
